4802ff20bfe47a9a2570381144cb4fd263e860f2
Maria Ozamiz <ozamiz.m@gmail.com>
2021-01-06 12:34:49 +0100

    use HTML reset instead of JS

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 'use strict';
 const letters = document.querySelectorAll('.letter');
-const clearButton = document.querySelector('.btn-clear');
-
-function clearAll() {
-    letters.forEach((letter) => {
-        letter.value = '';
-    });
-}
-
-clearButton.addEventListener('click', clearAll);
