05dc08beb967553e461df08e29d49d29215a2e35
Maria Ozamiz <ozamiz.m@gmail.com>
2021-01-07 15:02:59 +0100

    add a button to show errors

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 'use strict';
-const letters = document.querySelectorAll('.letter');
+const checkBtn = document.querySelector('.btn-check');
+let errorShown = false;
+
+function showErrors(e) {
+    e.preventDefault();
+    if (errorShown) {
+        document.documentElement.style.setProperty(
+            `--colorerror`,
+            `var(--blue)`
+        );
+        errorShown = false;
+    } else {
+        document.documentElement.style.setProperty(`--colorerror`, `red`);
+        errorShown = true;
+        checkBtn.innerHTML = `ocultar errores`;
+    }
+}
+
+checkBtn.addEventListener('click', showErrors);
