4ff19442e57ea69cc556231f1035134b0f35268c
Maria Ozamiz <ozamiz.m@gmail.com>
2021-01-08 19:30:33 +0100

    highlight rows or columns when focused, and highlight related clue

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 'use strict';
-const checkBtn = document.querySelector('.btn-check');
+let currentCell = '';
 let errorShown = false;
+let across = true;
+let clueText;
+const letters = document.querySelectorAll('.letter');
+const clues = document.querySelectorAll('.clue');
+const checkBtn = document.querySelector('.btn-check');
 
 function showErrors(e) {
     e.preventDefault();
@@ -17,4 +22,40 @@ function showErrors(e) {
     }
 }
 
+function handleClick(e) {
+    removeHighlights();
+    // check if we are at the same cell
+    if (currentCell === e.currentTarget.id) {
+        across = !across;
+    }
+    let selector = `[data-across="${e.currentTarget.dataset.across}"]`;
+    if (!across) {
+        selector = `[data-down="${e.currentTarget.dataset.down}"]`;
+    }
+    //highlight related letters
+    const letters = document.querySelectorAll(`input${selector}`);
+    letters.forEach((letter) => letter.classList.add('highlight'));
+
+    //highlight related clue
+    const clue = document.querySelector(`li${selector}`);
+
+    if (clue) {
+        clueText = clue.innerHTML;
+        clue.innerHTML = '<mark class="mark">' + clueText + '</mark>';
+    }
+    currentCell = e.currentTarget.id;
+}
+
+function removeHighlights() {
+    //remove highlight from letters
+    const highlightLetters = document.querySelectorAll('.highlight');
+    highlightLetters.forEach((letter) => letter.classList.remove('highlight'));
+    //remove highlight from text
+    const highlightClue = document.querySelector('.mark');
+    if (highlightClue) {
+        highlightClue.parentElement.innerHTML = clueText;
+    }
+}
+
 checkBtn.addEventListener('click', showErrors);
+letters.forEach((letter) => letter.addEventListener('click', handleClick));
