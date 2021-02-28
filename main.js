5a1dc16878f961fc3d1c0bad64176b4321593f29
Maria Ozamiz <ozamiz.m@gmail.com>
2021-02-28 20:55:32 +0100

    add max-height to clues container and scrollIntoView

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 'use strict';
+
+/*************************
+ *       Variables       *
+ *************************/
+
 const maxId = 168;
 let currentCellId = '';
 let errorShown = false;
@@ -18,7 +23,41 @@ const checkBtn = document.querySelector('.btn-check');
 const solveBtn = document.querySelector('.btn-solve');
 const clearBtn = document.querySelector('.btn-clear');
 
+/*************************
+ *       Listeners       *
+ *************************/
+
+table.addEventListener('mousedown', handleClick);
+table.addEventListener('input', handleWriting);
+table.addEventListener('keyup', handleKeyUp);
+checkBtn.addEventListener('click', showErrors);
+solveBtn.addEventListener('click', solveCrossword);
+clearBtn.addEventListener('click', clearGame);
+cells.forEach((cell) => cell.addEventListener('focus', handleFocus));
+
+/*************************
+ *         Start        *
+ *************************/
+
 startGame();
+
+function startGame() {
+    //check and paint localStorage data in DOM
+    if (Object.keys(crossword).length > 0) {
+        const keysArr = Object.keys(crossword);
+        keysArr.forEach((key) => {
+            let cell = document.getElementById(key);
+            cell.value = crossword[key];
+        });
+    }
+    if (totalSeconds > 0) {
+        setTime();
+    }
+}
+
+/*************************
+ *        Buttons        *
+ *************************/
 
 function showErrors() {
     if (errorShown) {
@@ -35,23 +74,6 @@ function showErrors() {
     }
 }
 
-function handleClick(e) {
-    if (currentCellId === e.target.id) {
-        across = !across;
-        handleFocus(e);
-    }
-}
-
-function handleFocus(e) {
-    removeHighligh();
-    let selector = `[data-across="${e.target.dataset.across}"]`;
-    if (!across) {
-        selector = `[data-down="${e.target.dataset.down}"]`;
-    }
-    addHighlight(selector);
-    currentCellId = e.target.id;
-}
-
 function solveCrossword() {
     cells.forEach((cell) => (cell.value = cell.pattern.charAt(1)));
 }
@@ -65,6 +87,24 @@ function clearGame() {
     setTime();
 }
 
+/*************************
+ *     LocalStorage      *
+ *************************/
+
+function saveCrossword(e) {
+    const id = e.target.id;
+    const letter = e.target.value;
+    //save crossword object: id as key, letter as value
+    crossword[id] = letter;
+    localStorage.setItem('crossword', JSON.stringify(crossword));
+    //save crossword timer
+    localStorage.setItem('timer', JSON.stringify(totalSeconds));
+}
+
+/*************************
+ *     Flow-writing     *
+ *************************/
+
 function handleWriting(e) {
     saveCrossword(e);
     if (!start) {
@@ -72,25 +112,26 @@ function handleWriting(e) {
     }
     let cellId = e.target.id;
     if (e.inputType === 'deleteContentBackward') {
+        return;
+    }
+    let nextCell = getNextCell(cellId);
+    while (!nextCell.hasAttribute(across ? 'data-across' : 'data-down')) {
+        nextCell = getNextCell(nextCell.id);
+    }
+    nextCell.focus();
+}
+
+function handleKeyUp(e) {
+    let cellId = e.target.id;
+    if (e.key === 'Backspace') {
         let previousCell = getPreviousCell(cellId);
-        console.log(previousCell);
         while (
             !previousCell.hasAttribute(across ? 'data-across' : 'data-down')
         ) {
             previousCell = getPreviousCell(previousCell.id);
         }
         previousCell.focus();
-    } else {
-        let nextCell = getNextCell(cellId);
-        while (!nextCell.hasAttribute(across ? 'data-across' : 'data-down')) {
-            nextCell = getNextCell(nextCell.id);
-        }
-        nextCell.focus();
     }
-}
-
-function handleKeyUp(e) {
-    let cellId = e.target.id;
     if (e.key === 'ArrowDown') {
         console.log('quiero bajar');
         let nextCellId = parseInt(cellId) + 12;
@@ -111,82 +152,6 @@ function handleKeyUp(e) {
 // if (e.key === 'ArrowRight') {
 //     console.log('quiero adelante');
 // }
-
-table.addEventListener('mousedown', handleClick);
-table.addEventListener('input', handleWriting);
-table.addEventListener('keyup', handleKeyUp);
-checkBtn.addEventListener('click', showErrors);
-solveBtn.addEventListener('click', solveCrossword);
-clearBtn.addEventListener('click', clearGame);
-cells.forEach((cell) => cell.addEventListener('focus', handleFocus));
-
-function addHighlight(selector) {
-    // at cells
-    const cells = document.querySelectorAll(`input${selector}`);
-    cells.forEach((cell) => cell.classList.add('highlight'));
-
-    // at clue
-    const clue = document.querySelector(`li${selector}`);
-    if (clue) {
-        clueText = clue.innerHTML;
-        clue.innerHTML = '<mark class="mark">' + clueText + '</mark>';
-    }
-}
-
-function removeHighligh() {
-    //from crossword cells
-    const highlightCell = document.querySelectorAll('.highlight');
-    highlightCell.forEach((cell) => cell.classList.remove('highlight'));
-    //from clue
-    const highlightClue = document.querySelector('.mark');
-    if (highlightClue) {
-        highlightClue.parentElement.innerHTML = clueText;
-    }
-}
-
-function saveCrossword(e) {
-    const id = e.target.id;
-    const letter = e.target.value;
-    //save crossword object: id as key, letter as value
-    crossword[id] = letter;
-    localStorage.setItem('crossword', JSON.stringify(crossword));
-    //save crossword timer
-    localStorage.setItem('timer', JSON.stringify(totalSeconds));
-}
-
-function startGame() {
-    //check and paint localStorage data in DOM
-    if (Object.keys(crossword).length > 0) {
-        const keysArr = Object.keys(crossword);
-        keysArr.forEach((key) => {
-            let cell = document.getElementById(key);
-            cell.value = crossword[key];
-        });
-    }
-    if (totalSeconds > 0) {
-        setTime();
-    }
-}
-
-function startTimer() {
-    start = true;
-    timer = setInterval(setTime, 1000);
-}
-
-function setTime() {
-    ++totalSeconds;
-    seconds.innerHTML = pad(totalSeconds % 60);
-    minutes.innerHTML = pad(parseInt(totalSeconds / 60));
-}
-
-function pad(val) {
-    var valString = val + '';
-    if (valString.length < 2) {
-        return '0' + valString;
-    } else {
-        return valString;
-    }
-}
 
 function getNextCell(id) {
     let cellId = parseInt(id);
@@ -214,4 +179,77 @@ function getPreviousCell(id) {
         }
     }
     return document.getElementById(cellId);
+}
+
+/************************************
+ *  Cell direction & highlighting   *
+ ************************************/
+
+function handleClick(e) {
+    if (currentCellId === e.target.id) {
+        across = !across;
+        handleFocus(e);
+    }
+}
+
+function handleFocus(e) {
+    removeHighligh();
+    let selector = `[data-across="${e.target.dataset.across}"]`;
+    if (!across) {
+        selector = `[data-down="${e.target.dataset.down}"]`;
+    }
+    addHighlight(selector);
+    currentCellId = e.target.id;
+}
+
+function addHighlight(selector) {
+    // at cells
+    const cells = document.querySelectorAll(`input${selector}`);
+    cells.forEach((cell) => {
+        cell.classList.add('highlight');
+        cell.parentElement.classList.add('highlight');
+    });
+
+    // at clue
+    const clue = document.querySelector(`li${selector}`);
+    if (clue) {
+        clueText = clue.innerHTML;
+        clue.innerHTML = '<mark class="mark">' + clueText + '</mark>';
+        clue.scrollIntoView();
+    }
+}
+
+function removeHighligh() {
+    //from crossword cells
+    const highlightCell = document.querySelectorAll('.highlight');
+    highlightCell.forEach((cell) => cell.classList.remove('highlight'));
+    //from clue
+    const highlightClue = document.querySelector('.mark');
+    if (highlightClue) {
+        highlightClue.parentElement.innerHTML = clueText;
+    }
+}
+
+/*************************
+ *         Timer         *
+ *************************/
+
+function startTimer() {
+    start = true;
+    timer = setInterval(setTime, 1000);
+}
+
+function setTime() {
+    ++totalSeconds;
+    seconds.innerHTML = pad(totalSeconds % 60);
+    minutes.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    var valString = val + '';
+    if (valString.length < 2) {
+        return '0' + valString;
+    } else {
+        return valString;
+    }
 }
